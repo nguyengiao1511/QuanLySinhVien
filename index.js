276ad85d7b45c3124c6bb78a19f443caa93fe4a4
nguyengiao1511 <nguyengiao1520@gmail.com>
2021-05-02 16:26:05 +0700

    tao_branch_devA

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log('Quản lý sinh viên');
 console.log('dự án sinh viên');
+function add(){
+    console.log('add');
+}
+function devA(){
+    console.log('dev_A code chức năng tạo branch');
+}
